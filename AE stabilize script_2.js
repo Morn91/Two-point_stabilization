81cfc56d609afca5a34d5de74587ec582b70c354
Sergey Lukyanov <kyanmorn@gmail.com>
2017-09-20 22:27:47 +0300

    ++

--- AE stabilize script_2.js
+++ AE stabilize script_2.js
@@ -1,10 +1,12 @@
 A = thisComp.layer("Null 1").transform.position;
 B = thisComp.layer("Null 2").transform.position;
 C = thisComp.layer("Camera 1").transform.position;
-alpha = -Math.atan2((A - C)[0], (A - C)[2]);
-beta = Math.atan2((A - C)[1], (A - C)[2]);
-X = B - C;
-X = [X[0], Math.cos(beta) * X[1] - Math.sin(beta) * X[2], Math.sin(beta) * X[1] + Math.cos(beta) * X[2]];
-X = [Math.cos(alpha) * X[0] + Math.sin(alpha) * X[2], X[1], -Math.sin(alpha) * X[0] + Math.cos(alpha) * X[2]];
-gamma =  Math.atan2(X[1], X[0]);
-[-beta / Math.PI * 180, -alpha / Math.PI * 180, gamma / Math.PI * 180]
+x = Math.atan2((A - C)[1], (A - C)[2]);
+y = -Math.atan2((A - C)[0], Math.sqrt(Math.pow((A - C)[1], 2) + Math.pow((A - C)[2], 2)));
+D = B - C;
+D = [D[0], Math.cos(x) * D[1] - Math.sin(x) * D[2], Math.sin(x) * D[1] + Math.cos(x) * D[2]];
+D = [Math.cos(y) * D[0] + Math.sin(y) * D[2], D[1], -Math.sin(y) * D[0] + Math.cos(y) * D[2]];
+y *= 180 / Math.PI
+x *= 180 / Math.PI
+z = Math.atan2(D[1], D[0]) * 180 / Math.PI;
+[-x, -y, z];
